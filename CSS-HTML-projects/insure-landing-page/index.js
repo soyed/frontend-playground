a2706def0d6b9a7042302380d12515b0baf5af72
Samuel Oyediran <samuel.oyediran00@gmail.com>
2022-03-05 22:01:12 -0500

    completed company motto, responsive across all screen sizes

--- CSS-HTML-projects/insure-landing-page/index.js
+++ CSS-HTML-projects/insure-landing-page/index.js
@@ -1,11 +1,2 @@
-//
-
-// => Grab Elements
 const menu = document.querySelector('.header__nav--menu');
 const menuIcon = document.querySelector('.hamburger--icon');
-
-if (menuIcon) {
-  menuIcon.addEventListener('click', (event) => {
-    console.log(event);
-  });
-}
