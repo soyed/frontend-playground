153ead4b5d432de550546a162f6804a25c76496e
Samuel Oyediran <samuel.oyediran00@gmail.com>
2022-03-14 19:06:30 -0400

    fully responsive navbar completed

--- js-micro-components/navbar-studio/01.navbar/index.js
+++ js-micro-components/navbar-studio/01.navbar/index.js
@@ -2,3 +2,9 @@
 const menuIcon = document.querySelector('.header--hamburger');
 const hamburger = document.querySelector('.header--hamburger div');
 const menu = document.querySelector('.header--nav');
+
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('open');
+  hamburger.classList.toggle('open');
+  menu.classList.toggle('open');
+});
