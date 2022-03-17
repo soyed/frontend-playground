c1ca95fa8cb1dd3ab282bf20663a36de1076522e
Samuel Oyediran <samuel.oyediran00@gmail.com>
2022-03-17 18:09:44 -0400

    completed navbar for insure landing page

--- CSS-HTML-projects/insure-landing-page/index.js
+++ CSS-HTML-projects/insure-landing-page/index.js
@@ -1,2 +1,15 @@
-const menu = document.querySelector('.header__nav--menu');
-const menuIcon = document.querySelector('.hamburger--icon');
+const menu = document.querySelector('.header__nav');
+const menuIcon = document.querySelector('.header__hamburger img');
+
+menuIcon.addEventListener('click', () => {
+  const currentIcon = menuIcon.src.includes('icon-hamburger');
+
+  if (currentIcon) {
+    menuIcon.src = './images/icon-close.svg';
+  } else {
+    menuIcon.src = './images/icon-hamburger.svg';
+  }
+  // => toggle menu bar
+  menuIcon.classList.toggle('open');
+  menu.classList.toggle('open');
+});
