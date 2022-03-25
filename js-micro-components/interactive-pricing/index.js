b35af90c50b8f377063ce160bdf39f1cd87d6dc4
Samuel Oyediran <samuel.oyediran00@gmail.com>
2022-03-24 21:50:31 -0400

    completed responsive price functionality

--- js-micro-components/interactive-pricing/index.js
+++ js-micro-components/interactive-pricing/index.js
@@ -5,28 +5,51 @@ const priceFill = document.querySelector('.price__fill');
 const priceTrack = document.querySelector('.price__track');
 
 /*==========  Helper Methods  ======*/
+let clicked = false,
+  toggled = false;
+const parseDefaultPrice = () => {
+  return priceText.textContent.slice(1).split('.')[0];
+};
+
+const parsePrice = (price) => {
+  if (priceText.dataset.month === 'month')
+    return (price + (price * 0.25) / 0.75) / 12;
+  else if (priceText.dataset.month === 'year') return price * 12 * 0.75;
+};
+
+const togglePrice = (price = parseDefaultPrice()) => {
+  price = Number(price);
+  priceText.textContent = `$${parsePrice(price)}.00`;
+};
+const updatePrice = (price) => {
+  if (priceText.dataset.month === 'year') {
+    priceText.textContent = `$${parsePrice(price)}.00`;
+  } else {
+    priceText.textContent = `$${price}.00`;
+  }
+};
 
 const slidePrice = (event) => {
   const { offsetX } = event;
-  const width = Math.abs((offsetX / priceTrack.clientWidth) * 100);
-  console.log(width);
-  priceFill.style.width = `${width}%`;
+  const width = Math.round((offsetX / priceTrack.clientWidth) * 100);
+  updatePrice(width);
+  priceFill.style.width = `calc(${width}% - 1.75rem)`;
 };
 
-const togglePrice = () => {
-  // => toggle price from monthly to yearly
-  // Price range goes from 0 to 100 for monthly
-  // yearly price = (monthly_price * 12) * 25% discount
+const handleTogglePrice = () => {
+  toggleBtn.classList.toggle('toggle');
+  if (priceText.dataset.month === 'month') {
+    priceText.setAttribute('data-month', 'year');
+  } else {
+    priceText.setAttribute('data-month', 'month');
+  }
+  togglePrice();
 };
 
 /*==========  Event Listeners  ======*/
 
 // => List for price toggle => monthly / yearly
-toggleBtn.addEventListener('click', () => {
-  toggleBtn.classList.toggle('toggle');
-});
-
-// => track on price track click
+toggleBtn.addEventListener('click', handleTogglePrice);
 
 // => track mousemove when the price slider is clicked
 let mousedown = false;
@@ -34,10 +57,5 @@ let mousedown = false;
 priceSlider.addEventListener('mouseup', () => (mousedown = false));
 priceSlider.addEventListener('mousedown', () => (mousedown = true));
 
-priceTrack.addEventListener('mousemove', (event) => {
-  //   if (mousedown) {
-  //     console.log(event);
-  //   }
-});
-
+priceTrack.addEventListener('mousemove', (event) => {});
 priceTrack.addEventListener('click', slidePrice);
